1ca5b1fb12dabf07e75138759e8147d40af1137d
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-11-29 17:19:26 -0300

    commit 2 29.11.2024

--- JavaScript/Jokenpo/scripts/GameLogic.js
+++ JavaScript/Jokenpo/scripts/GameLogic.js
@@ -1,25 +1,96 @@
 player = "";
 computer = "";
 
+playerImageURL = document.getElementById("playerResult");
+computerImageURL = document.getElementById("computerResult");
+
 // 1 para Pedra / 2 para Papel / 3 para Tesoura
 playerNumber = 0;
-randomNumber = 0;
+computerNumber = 0;
 
 // Elementos do html
 response = document.getElementById("response");
 
-function PlayerChoice(_number) {
-    playerNumber = _number;
+// Imagens da resposta
+crown = document.getElementById("crown");
+skull = document.getElementById("skull");
 
-    switch (_number) {
+
+function Start(_playerChoice) {
+    // Escolha do Jogador
+    playerNumber = _playerChoice;
+    PlayerChoice();
+
+    // Escolha do Computador
+    computerNumber = Math.floor(Math.random() * 3 ) + 1; // Geração de um número aleatório
+    ComputerChoice();
+
+    console.log(`Escolha do Jogador: ${player}\nEscolha do Computador: ${computer}`);
+
+
+    // Lógica do Jogo
+    // Decidir o vencedor e o perdedor
+    let win = false;
+
+    // Testar todas as possibilidades onde o jogador é o vencedor. Ao final, exibir uma mensagem na tela.
+    if ( player == "Pedra" && computer == "Tesoura" )
+        win = true;
+
+    if ( player == "Papel" && computer == "Pedra" )
+        win = true;
+
+    if ( player == "Tesoura" && computer == "Papel" )
+        win = true;
+
+
+    // Exibir resposta (output)
+    if (win == true)
+    {
+        response.textContent = `Jogador Venceu !!  ${player} x ${computer}`;
+
+        crown.style.display = "block";
+        skull.style.display = "none";
+    }
+    else
+    {
+        response.textContent = `Jogador Perdeu F  ${player} x ${computer}`;
+
+        skull.style.display = "block";
+        crown.style.display = "none";
+    }
+}
+
+
+function PlayerChoice() {
+    switch (playerNumber) {
         case 1:
             player = "Pedra";
+            playerImageURL.src = "../Images/rock.png"
             break;
         case 2:
             player = "Papel";
+            playerImageURL.src = "../Images/paper.png";
             break;
         case 3:
             player = "Tesoura";
+            playerImageURL.src = "../Images/scissor.png";
+            break;
+    }
+}
+
+function ComputerChoice() {
+    switch (computerNumber) {
+        case 1:
+            computer = "Pedra";
+            computerImageURL.src = "../Images/rock.png"
+            break;
+        case 2:
+            computer = "Papel";
+            computerImageURL.src = "../Images/paper.png";
+            break;
+        case 3:
+            computer = "Tesoura";
+            computerImageURL.src = "../Images/scissor.png";
             break;
     }
 }
