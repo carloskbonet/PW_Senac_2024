c4fad4e752f3c0d35139a7c84ef68951df6cc4a6
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-12-13 14:10:00 -0300

    commit 1 13.12.2024

--- Vetores/Exercicio9.py
+++ Vetores/Exercicio9.py
@@ -2,21 +2,29 @@ numbers = [ 1 , 9 , 3 , 0 , 4 , 7 , 2 , 100 , 3 , 2 ];
 
 # Ordenação / Verificação
 
-
 # Solução 1
+for i in range(1, len(numbers)):
+    # Verificar com o valor da posição anterior
+    if ( numbers[i] < numbers[i - 1] ):
+        print(f"Não está ordenado.\n{numbers}\n");
+        numbers.sort();
+        print(f"Vetor ordenado: \n{numbers}");
+        exit();
+
+
+
+# Se chegar até aqui significa que o vetor está ordenado
+print(f"O vetor está corretamente ordenado.\n{numbers}");
+
+
+
+
+# Solução 2
 
 # vectorCopy = numbers.copy();
-
 # vectorCopy.sort();
 
 # if (numbers == vectorCopy):
 #     print("Está Ordenado.");
 # else:
 #     print(f"Não está ordenado.\nFormato ordenado: {vectorCopy}")
-
-
-# Solução 2
-for i in range(0, len(numbers)):
-    # Verificar se está ordenado
-    # Podemos armazenar o número da verificação anterior.
-    # Se o numbers[i] for menor que o número anterior, o vetor não está ordenado
